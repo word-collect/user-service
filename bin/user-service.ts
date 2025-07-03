89225ef908154223e56015279dc7327de2279113
rooster man <roostermanred@gmail.com>
2025-07-03 13:06:59 -0500

    add auth stack

---
+++ bin/user-service.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib'
+import { AuthStack } from '../lib/auth-stack'
+
+const app = new cdk.App()
+
+const appName = 'word-collect'
+const environment = app.node.tryGetContext('environment') || 'dev'
+
+const authStack = new AuthStack(app, `${appName}-${environment}-auth-stack`, {
+  appName,
+  environment,
+  description: 'Auth stack for user service',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
+
+// Add tags to all stacks
+const tags = {
+  Environment: environment,
+  Service: 'user-service',
+  Application: appName
+}
+
+Object.entries(tags).forEach(([key, value]) => {
+  cdk.Tags.of(authStack).add(key, value)
+})
---
+++ lib/auth-stack.ts
@@ -0,0 +1,42 @@
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as cognito from 'aws-cdk-lib/aws-cognito'
+import * as ssm from 'aws-cdk-lib/aws-ssm'
+
+export interface AuthStackProps extends StackProps {
+  appName: string
+  environment: string
+}
+
+export class AuthStack extends Stack {
+  constructor(scope: Construct, id: string, props: AuthStackProps) {
+    super(scope, id, props)
+    const { appName, environment } = props
+
+    const userPool = new cognito.UserPool(this, 'UserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      passwordPolicy: { minLength: 12, requireSymbols: true },
+      removalPolicy: RemovalPolicy.DESTROY // DEV ONLY
+    })
+
+    const client = new cognito.UserPoolClient(this, 'AppClient', {
+      userPool,
+      generateSecret: false,
+      authFlows: { userPassword: true, userSrp: true }
+    })
+
+    // ---- surface the IDs ----
+    new ssm.StringParameter(this, 'UserPoolIdParam', {
+      parameterName: `/${appName}/${environment}/cognito/userPoolId`,
+      stringValue: userPool.userPoolId
+    })
+    new ssm.StringParameter(this, 'AppClientIdParam', {
+      parameterName: `/${appName}/${environment}/cognito/appClientId`,
+      stringValue: client.userPoolClientId
+    })
+
+    new CfnOutput(this, 'UserPoolId', { value: userPool.userPoolId })
+    new CfnOutput(this, 'AppClientId', { value: client.userPoolClientId })
+  }
+}
